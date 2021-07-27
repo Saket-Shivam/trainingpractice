b26942effea4458e70eea1ebea5c6684e98a2fd6
Saket-Shivam <42244200+Saket-Shivam@users.noreply.github.com>
2021-07-27 21:55:59 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,154 @@
+/*let i;
+i=10;
+console.log(i+"outside the block");
+if(i>5){
+    let i=2;
+    console.log(i+"in local");
+   }
+console.log(i+"after local");
+//variable hoisting;
+var a
+let b
+const c=1
+console.log("value of a is",a)
+console.log("value of b is",b)
+console.log("value of c is",c)
+//backticks:-special function $(1+2+2)*/
+
+//var student=["ram","mahesh","Sharda"]
+//c/onsole.log(student[1])
+
+
+
+/*x=10
+String(10)
+console.log(typeof(String(625+1)))
+console.log(typeof(x.toString()))
+console.log(typeof((625).toString()))
+//toExp
+//toFixed
+//toPrecision
+x=true
+console.log(typeof(x))
+console.log(typeof(Number(x)))
+*/
+
+/*var readline=require('readline');
+
+var a=readline.createInterface(process.stdin,process.stdout);
+a.question("what is your name?",(name)=>{
+    console.log(name);
+    a.close();
+});
+*/
+/*var a=8;
+var b=3;
+a+=++a +a++ + ++a + a++ + a++ + ++a
+console.log(a)*/
+
+/*var totalMarks=100
+var obtained=65
+if(obtained>=60)
+{
+    console.log("First Division....")
+} 
+else if(obtained<60 && obtained>=45)
+{
+    console.log("Second division....")
+}
+else if(obtained<45 && obtained>=33)
+{
+    console.log("Third Division....")
+}
+else{
+    console.log("failed....")
+}*/
+
+/*const a=true,b=false;
+console.log(a || b)
+console.log(!a)
+console.log(a&&a)*/
+/*a=10;
+b=a?'100':0;
+console.log(b)
+
+var c=102
+var d=c>200?"bad":c>150?"average":c>=100?"safe":"fail;"
+console.log(d)
+
+x=(2,3)
+console.log(x)*/
+
+/*var result
+var operation='+';
+var a=2
+var b=3
+switch(operation){
+    case '+': result=a+b;break;
+    case '-': result=a-b;break;
+    case '*':result=a*b;break;
+    case '/': result=a/b;break;
+    default: console.log("bye bye")
+}
+console.log(result)
+
+for(var i=0;i<10;i++)
+{
+    console.log(i)
+}
+
+//for-in loop
+var listnew=[1,2,3,4]
+for(var x in  listnew){
+    console.log(listnew[x])
+}*/
+//for-of loop
+//var listnew=[1,2,3,4]
+//for(var x of  listnew){
+   // console.log(listnew)
+//}
+
+/*var r=6
+for(var i=1;i<=r;i++)
+{
+    for(var j=1;j<=i;j++){
+        console.log("*")
+    }
+}*/
+
+/*a=4
+b=c
+for(var i=1;i<=a;i++){
+    for(var j=1;j<=name;j++){
+        console.log("")
+    }
+    for(var k=1;k<=2*i-1;k++)
+    {
+        if(k==1 || k==2*i-1||i==n)
+        console.log("*")
+        else
+        console.log("")
+    }
+}*/
+var a=null??'bye';
+console.log(a)
+
+
+function myAddition(a,b){
+    var c=a+b;
+    return c
+
+}
+c=myAddition(1,2)
+console.log(c)
+function percentage()
+
+
+
+
+
+
+
+
+
+
